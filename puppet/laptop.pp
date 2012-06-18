61271fec9b5cec76281b80bd7bd666616d932138
Han Kai <isaachanstar@gmail.com>
2012-06-19 09:38:22 +1000

    install more software

---
+++ puppet/laptop.pp
@@ -0,0 +1,35 @@
+package { 'emacs23' :
+    ensure => present,
+}
+
+package { 'openjdk-6-jdk':
+    ensure => present,
+}
+
+package { 'maven2':
+    ensure => present,
+}
+
+package { 'ant1.7':
+    ensure => present,
+}
+
+package { 'groovy':
+    ensure => present,
+}
+
+package { 'gradle':
+    ensure => present,
+}
+
+package { 'mercurial':
+    ensure => present,
+}
+
+package { 'subversion':
+    ensure => present,
+}
+
+package { 'swi-prolog-nox':
+    ensure => present,
+}
